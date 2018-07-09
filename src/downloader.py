41ffa2ebe970f851e99246738571372755872ca6
MarkKoz <KozlovMark@gmail.com>
2018-07-09 12:57:09 -0700

    add type annotations

--- src/downloader.py
+++ src/downloader.py
@@ -1,17 +1,20 @@
 from argparse import ArgumentParser
+from typing import Generator, Union
 from urllib.parse import urlparse
 import math
-import requests
 import sys
+
+import requests
 
 VERSION = "0.1.0"
 QUERY_ENDPOINT = "https://osusearch.com/query"
 DOWNLOAD_BASE = "https://osu.ppy.sh/beatmapsets"
 
-def get_download_urls(query: str, offset: int):
+def get_download_urls(query: str, offset: int) \
+        -> Generator[Union[int, str], None, None]:
     response = requests.get(f"{QUERY_ENDPOINT}/?{query}&offset={offset}")
     response.raise_for_status() # TODO: Handle raised exceptions.
-    data = response.json()
+    data: dict = response.json()
 
     if offset == 0:
         yield int(data["result_count"])
@@ -19,14 +22,14 @@ def get_download_urls(query: str, offset: int):
     for beatmap in data["beatmaps"]:
         yield f"{DOWNLOAD_BASE}/{beatmap['beatmapset_id']}/download?noVideo=1"
 
-def get_all_urls(url: str):
-    query = urlparse(url).query
+def get_all_urls(url: str) -> Generator[str, None, None]:
+    query: str = urlparse(url).query
 
     # Offset 0 yields result_count first.
     first_resp = get_download_urls(query, 0)
 
     # Each request returns at most 18 beatmaps.
-    max_offset = math.ceil(next(first_resp) / 18)
+    max_offset: int = math.ceil(next(first_resp) / 18)
 
     yield from first_resp # The remainder of the generator contains the urls.
 
